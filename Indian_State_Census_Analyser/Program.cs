059bb15f85439dabe03ac750c0f3a36a4669e3a5
Harshita <chandaliyaharshita222@gmail.com>
2023-06-01 22:08:56 +0530

    Added Welcome Message

---
+++ Indian_State_Census_Analyser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Indian_State_Census_Analyser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian State Census Analyser Problem!");
+        }
+    }
+}
